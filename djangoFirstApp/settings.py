052d8431a54e7fb599e98b12c331bbb4611ee272
Jaganprafful <67191031+Jaganprafful@users.noreply.github.com>
2021-08-24 10:26:38 +0530

    Update settings.py

--- djangoFirstApp/settings.py
+++ djangoFirstApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*u(u8qbw817@$#@3_k7p^pn35%s#lk@oa7raxzs#f)aknyoqz='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
